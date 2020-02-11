978b7ad74ef7a5ba30e32bbfe7652a9674d7f04c
Takuya Kodama <takuyakodama@r.recruit.co.jp>
2020-02-12 00:19:38 +0900

    14-avoiding-recursive-logins

--- src/main/kotlin/dev/kakueki61/command/LoginCommand.kt
+++ src/main/kotlin/dev/kakueki61/command/LoginCommand.kt
@@ -3,6 +3,7 @@ package dev.kakueki61.command
 import dev.kakueki61.Database
 import dev.kakueki61.Outputter
 import dev.kakueki61.di.UserCommandsRouter
+import java.util.*
 import javax.inject.Inject
 
 class LoginCommand
@@ -10,17 +11,22 @@ class LoginCommand
 constructor(
     private val db: Database,
     private val outputter: Outputter,
-    private val userCommandsRouterFactory: UserCommandsRouter.Factory
+    private val userCommandsRouterFactory: UserCommandsRouter.Factory,
+    private val account: Optional<Database.Account>
 ) : SingleArgCommand() {
     init {
         println("Creating a new $this")
     }
 
     override fun handleArg(userName: String): Command.Result {
-        val account = db.getAccount(userName)
-        println("$userName is logged in with balance: ${account.balance}")
+        if (account.isPresent) {
+            return Command.Result.handled()
+        }
+
+        val newAccount = db.getAccount(userName)
+        println("$userName is logged in with balance: ${newAccount.balance}")
         return Command.Result.enterNestedCommandSet(
-            userCommandsRouterFactory.create(account).router()
+            userCommandsRouterFactory.create(newAccount).router()
         )
     }
 }
--- src/main/kotlin/dev/kakueki61/di/LoginCommandModule.kt
+++ src/main/kotlin/dev/kakueki61/di/LoginCommandModule.kt
@@ -1,15 +1,21 @@
 package dev.kakueki61.di
 
 import dagger.Binds
+import dagger.BindsOptionalOf
 import dagger.Module
 import dagger.multibindings.IntoMap
 import dagger.multibindings.StringKey
+import dev.kakueki61.Database
 import dev.kakueki61.command.Command
 import dev.kakueki61.command.LoginCommand
 
 @Module
 abstract class LoginCommandModule {
-    @Binds @IntoMap
+    @Binds
+    @IntoMap
     @StringKey("login")
     abstract fun loginCommand(loginCommand: LoginCommand): Command
+
+    @BindsOptionalOf
+    abstract fun optionalAccount(): Database.Account
 }
